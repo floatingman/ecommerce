5fbbc10a6ae0472de5ad8924f8eb649f573bf4e2
Daniel Newman <dwnewman78@gmail.com>
2016-02-03 01:04:44 +0000

    initial commit

---
+++ routes/main.js
@@ -0,0 +1,30 @@
+var router = require('express').Router();
+var User = require('../models/user');
+
+router.get('/', function(req, res){
+  res.render('main/home');
+});
+
+router.get('/about', function(req, res){
+  res.render('main/about');
+});
+
+router.get('/users', function(req, res){
+  User.find({}, function(err, users){
+    res.json(users);
+  });
+});
+
+router.get('/products/:id', function(req, res, next){
+  Product
+    .find({ category: req.params.id})
+    .populate('category')
+    .exec(function(err, products){
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products
+      });
+    });
+});
+
+module.exports = router;
